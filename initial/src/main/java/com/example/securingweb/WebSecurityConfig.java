7913692b16aa750cd0301eb8f52ce4d068dc57a3
DartSuslik42 <marinovich.ml@phystech.edu>
2021-09-28 01:16:38 +0300

    Developing Commit:     - all security-required pages URL started with /app/     - added new //TODO: quests     - new functional: /app/users/{id} - search user by id. Cosmetic Commit:     - removed registration.html as no-needed
    Signed-off-by: DartSuslik42 <marinovich.ml@phystech.edu>

--- initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -47,12 +47,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/users").authenticated()
+                .antMatchers("/app/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .usernameParameter("email")
-                .defaultSuccessUrl("/users")
+                .defaultSuccessUrl("/app/users")
                 .permitAll()
                 .and()
                 .logout().logoutSuccessUrl("/").permitAll();
--- initial/src/main/java/com/example/securingweb/accessingdatamysql/UserController.java
+++ initial/src/main/java/com/example/securingweb/accessingdatamysql/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 
 @AllArgsConstructor /* Конструктор всех полей класса.
                        Добавлен что-бы создать DependencyInjection в поле UserRepository.
@@ -25,6 +28,8 @@ public class UserController {
 
     private UserRepository userRepository;
 
+    //TODO: Сделать переадресацию на рукописную страницу авторицзации login.html
+
     @GetMapping("")
     public String viewHomePage(){
         return "index";     // переход к index.html
@@ -38,6 +43,8 @@ public class UserController {
         return "signup_form";   // переход к signup_form.html
     }
 
+    // TODO: при переходе на index.html высвечивать надпись об успешной регистрации
+    // TODO: при некоректно ведённых данных переходить на signup_form.html с объяснением ошибки
     @PostMapping("/process_register")
     public String processRegister(User user) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // Создаём кодировщик
@@ -46,26 +53,36 @@ public class UserController {
 
         userRepository.save(user); // Сохраняем пользователя в БД
 
-        return "register_success"; // переход к register_success.html
+    //  return "register_success"; // переход к register_success.html
+        return "index"; // переход к index.html
     }
 
-    @GetMapping("/users")
+    @GetMapping("/app/users")
     public String listUsers(Model model) {
-        List<User> listUsers = userRepository.findAll(); // Возвращает всё содержимое базы данных
+        List<User> listUsers; // Возвращает всё содержимое базы данных
+        listUsers = userRepository.findAll();
         model.addAttribute("listUsers", listUsers); // Нужно для обработки на уровне HTML
 
         return "users"; // переход к users.html
     }
 
-//    @GetMapping(path="/users/{id}")
-//    public @ResponseBody ResponseEntity<User> getUserByID(@PathVariable Integer id) {
-//        Optional<User> user = userRepository.findById(id);
-//        if(user.isPresent()) {
-//            return new ResponseEntity<User>(user.get(), HttpStatus.OK);
-//        }
-//        else {
-//            throw new NoSuchElementException();
-//        }
-//    }
+    @GetMapping("/app/users/{id}")
+    public String getUserByID(@PathVariable Integer id, Model model) {
+
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()) {
+            // Если пользователь найден
+            List<User> users = new ArrayList<User>();
+            users.add(user.get());
+            model.addAttribute("listUsers",users);
+            return "users";
+        }
+        else {
+            // Если пользователь не найден
+            model.addAttribute("listUsers", new ArrayList<>());
+            return "users";
+        }
+    }
 
 }
